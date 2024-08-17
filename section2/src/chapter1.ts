917d7e87292f4acbeac33f08c103f9552603256d
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-08-17 19:58:19 +0900

    primitive type

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,34 @@
+// number
+let num1: number = 123; // type anntation
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = NaN;
+
+// string
+let str1: string = "hello";
+let str2: string = 'hello';
+let str3: string = `hello`;
+let str4: string = `hello${num1}`;
+
+// boolean
+let boo1: boolean = true;
+
+// null
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+// let numA: number = null; 
+// strictNullChecks를 false로 하면 임시 null 값 저장 가능
+
+// 리터럴 타입
+// 리터럴 -> 값
+let numA: 10 = 10;
+numA = 12;
+
+let strA: "hello" = "hello";
+
+let boolA: true = false;
