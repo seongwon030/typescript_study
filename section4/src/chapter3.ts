7a8080815e13649139f3c079c30ccfa9c6156018
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-01-07 14:04:00 +0900

    함수 오버로딩

---
+++ section4/src/chapter3.ts
@@ -0,0 +1,22 @@
+/**
+ * 함수 오버로딩
+ * 하나의 함수를 매개변수의 개수나 타입에 따라
+ * 여러가지 버전으로 만드는 문법
+ * -> 하나의 함수 func
+ * -> 모든 매개변수의 타입 Number
+ * -> 매개변수 1개 -> 이 매개변수에 20을 곱한 값 출력
+ * -> 매개변수 3개 -> 이 매개변수들을 다 더한 값 출력
+ */
+
+// 버전들 -> 오버로드 시그니처
+function func(a: number): void;
+function func(a: number, b: number, c: number): void;
+
+// 실제 구현부 -> 구현 시그니처
+function func(a: number, b?: number, c?: number) {
+  if (typeof b === "number" && typeof c === "number") {
+    console.log(a + b + c);
+  } else {
+    console.log(a * 20);
+  }
+}
